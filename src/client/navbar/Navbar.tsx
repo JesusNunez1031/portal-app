8050436eb27705d3876559c5badd48b513064fa9
Jesus Nunez <jesusnunez1343@gmail.com>
2025-03-05 22:23:01 -0800

    init app

---
+++ src/client/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import { AppBar } from "@mui/material"
+
+
+const Navbar: React.FC = () => {
+    return (
+        <AppBar position="static">
+            
+        </AppBar>
+    )
+}
+
+export default Navbar;
